5991fe0684c5d28bdc404a3b975bf752bbb93024
kobederidder <kobe_de_ridder@hotmail.com>
2017-05-05 12:51:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kobe/notedroidmobapp10/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.kobe.notedroidmobapp10.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kobe.notedroidmobapp10.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
